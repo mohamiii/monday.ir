b17df4df9d13edb38a0122f8fcdf37c30603a917
mohamiii <moam3m@gmail.com>
2024-02-27 19:40:49 +0330

    Added Email validation Added password strength check

--- Back-end/accounts/serializers.py
+++ Back-end/accounts/serializers.py
@@ -2,21 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-def validate_email(value):
-    lower_email = value.lower()
-    if User.objects.filter(email__iexact=lower_email).exists():
-        raise serializers.ValidationError("Email exists!")
-    if "@gmail" not in value:
-        raise serializers.ValidationError("Only Gmail!")
-    return lower_email
-
-
-def validate_username(value):
-    if value == 'admin':
-        raise serializers.ValidationError('Username can\'t be `admin`')
-    return value
-
-
 class UserRegisterSerializer(serializers.ModelSerializer):
     passwordConfirm = serializers.CharField(write_only=True, required=True)
     email = serializers.EmailField(required=True)
@@ -26,8 +11,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'passwordConfirm')
         extra_kwargs = {
             'password': {'write_only': True},
-            'email': {'validators': (validate_email,)},
-            'username': {'validators': (validate_username,)}
         }
 
     def create(self, validated_data):
@@ -37,4 +20,31 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if data['password'] != data['passwordConfirm']:
             raise serializers.ValidationError('password does not match')
+
+        min_length = 8
+
+        if len(data['password']) < min_length:
+            raise serializers.ValidationError('Password must be at least {0} characters long.'.format(min_length))
+
+        # check for digit
+        if not any(char.isdigit() for char in data['password']):
+            raise serializers.ValidationError('Password must contain at least 1 digit.')
+
+        # check for letter
+        if not any(char.isalpha() for char in data['password']):
+            raise serializers.ValidationError('Password must contain at least 1 letter.')
+
         return data
+
+    def validate_email(self, value):
+        lower_email = value.lower()
+        if User.objects.filter(email__iexact=lower_email).exists():
+            raise serializers.ValidationError("Email exists!")
+        if "@gmail" not in value:
+            raise serializers.ValidationError("Only Gmail allowed!")
+        return lower_email
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('Username can\'t be `admin`')
+        return value
